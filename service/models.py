de5721e56546febab21908f2376ee223341cc568
Hanhlin <hanhsuan.lin0615@gmail.com>
2023-03-02 22:19:36 +0000

    initial version of db model

--- service/models.py
+++ service/models.py
@@ -1,9 +1,29 @@
 """
-Models for YourResourceModel
+Models for Products
 
 All of the models are stored in this module
+
+Models
+------
+Product - A Product in a catalog
+
+Attributes:
+-----------
+name (string) - the name of the product
+desc (string) - the description of the product
+price (float) - the price of the product
+category (string) - the category the product belongs to (i.e., tops, shirts)
+inventory (int) - the quantity of the product
+discount (float) - the discount percent of the product (i.e., from 1.0 to 0.1)
+created_date (timestamp) - the timestamp when the product is created
+modified_date (timestamp) - the timestamp when the product is modified
+deleted_date (timestamp) - the timestamp when the product is deleted
+
 """
 import logging
+# from enum import Enum
+from datetime import date
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 logger = logging.getLogger("flask.app")
@@ -15,76 +35,125 @@ db = SQLAlchemy()
 # Function to initialize the database
 def init_db(app):
     """ Initializes the SQLAlchemy app """
-    YourResourceModel.init_db(app)
+    Product.init_db(app)
 
 
 class DataValidationError(Exception):
     """ Used for an data validation errors when deserializing """
 
 
-class YourResourceModel(db.Model):
+class Product(db.Model):
     """
-    Class that represents a YourResourceModel
+    Class that represents a Product
     """
 
     app = None
 
+    ##################################################
     # Table Schema
+    ##################################################
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
+    name = db.Column(db.String(63), nullable=False)
+    desc = db.Column(db.String(256))
+    price = db.Column(db.Float(), nullable=False)
+    category = db.Column(db.String(63), nullable=False)
+    inventory = db.Column(db.Integer(), nullable=False)
+    discount = db.Column(db.Float(), nullable=False, default=1)
+    created_date = db.Column(db.Date(), nullable=False, default=date.today())
+    modified_date = db.Column(db.Date())
+    deleted_date = db.Column(db.Date())
+
+    ##################################################
+    # INSTANCE METHODS
+    ##################################################
 
     def __repr__(self):
-        return f"<YourResourceModel {self.name} id=[{self.id}]>"
+        return f"<Product {self.name} id=[{self.id}]>"
 
     def create(self):
         """
-        Creates a YourResourceModel to the database
+        Creates a Product to the database
         """
-        logger.info("Creating %s", self.name)
+        logger.info("Creating product %s", self.name)
         self.id = None  # pylint: disable=invalid-name
         db.session.add(self)
         db.session.commit()
 
     def update(self):
         """
-        Updates a YourResourceModel to the database
+        Updates a Product to the database
         """
         logger.info("Saving %s", self.name)
+        if not self.id:
+            raise DataValidationError("Update called with empty ID field")
         db.session.commit()
 
     def delete(self):
-        """ Removes a YourResourceModel from the data store """
-        logger.info("Deleting %s", self.name)
+        """ Removes a Product from the data store """
+        logger.info("Deleting product %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self):
-        """ Serializes a YourResourceModel into a dictionary """
-        return {"id": self.id, "name": self.name}
+        """ Serializes a Product into a dictionary """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "desc": self.desc,
+            "price": self.price,
+            "category": self.category,
+            "inventory": self.inventory,
+            "discount": self.discount,
+            "created_date": self.created_date.isoformat(),
+            "modified_date": self.modified_date.isoformat(),
+            "deleted_date": self.deleted_date.isoformat()
+        }
 
     def deserialize(self, data):
         """
-        Deserializes a YourResourceModel from a dictionary
+        Deserializes a Product from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
         """
         try:
+            # self.id = data["id"]
             self.name = data["name"]
+            if "desc" in data:
+                self.desc = data["desc"]
+            self.price = data["price"]
+            self.category = data["category"]
+            self.inventory = data["inventory"]
+            self.discount = data["discount"]
+            self.created_date = date.fromisoformat(data["created_date"])
+            if "modified_date" in data:
+                self.modified_date = date.fromisoformat(data["modified_date"])
+            if "deleted_date" in data:
+                self.deleted_date = date.fromisoformat(data["deleted_date"])
+        except AttributeError as error:
+            raise DataValidationError("Invalid attribute: " + error.args[0]) from error
         except KeyError as error:
-            raise DataValidationError(
-                "Invalid YourResourceModel: missing " + error.args[0]
-            ) from error
+            raise DataValidationError("Invalid product: missing " + error.args[0]) from error
         except TypeError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: body of request contained bad or no data - "
-                "Error message: " + error
+                "Invalid product: body of request contained bad or no data - "
+                "Error message: " + str(error)
             ) from error
         return self
 
+    ##################################################
+    # CLASS METHODS
+    ##################################################
+
     @classmethod
-    def init_db(cls, app):
-        """ Initializes the database session """
+    def init_db(cls, app: Flask):
+        """ Initializes the database session
+
+        :param app: the Flask app
+        :type data: Flask
+
+        """
         logger.info("Initializing database")
         cls.app = app
         # This is where we initialize SQLAlchemy from the Flask app
@@ -93,23 +162,77 @@ class YourResourceModel(db.Model):
         db.create_all()  # make our sqlalchemy tables
 
     @classmethod
-    def all(cls):
-        """ Returns all of the YourResourceModels in the database """
-        logger.info("Processing all YourResourceModels")
+    def all(cls) -> list:
+        """ Returns all of the Product in the database """
+        logger.info("Processing all Products")
         return cls.query.all()
 
     @classmethod
-    def find(cls, by_id):
-        """ Finds a YourResourceModel by it's ID """
-        logger.info("Processing lookup for id %s ...", by_id)
-        return cls.query.get(by_id)
+    def find(cls, product_id: int):
+        """ Finds a Product by it's ID
+
+        :param product_id: the id of the Product to find
+        :type product_id: int
+
+        :return: an instance with the product_id, or None if not found
+        :rtype: Product
+
+        """
+        logger.info("Processing lookup for product id %s ...", product_id)
+        return cls.query.get(product_id)
 
     @classmethod
-    def find_by_name(cls, name):
-        """Returns all YourResourceModels with the given name
+    def find_or_404(cls, product_id: int):
+        """Find a Product by it's id
 
-        Args:
-            name (string): the name of the YourResourceModels you want to match
+        :param product_id: the id of the Product to find
+        :type product_id: int
+
+        :return: an instance with the product_id, or 404_NOT_FOUND if not found
+        :rtype: Product
+
         """
-        logger.info("Processing name query for %s ...", name)
+        logger.info("Processing lookup or 404 for product id %s ...", product_id)
+        return cls.query.get_or_404(product_id)
+
+    @classmethod
+    def find_by_name(cls, name: str) -> list:
+        """Returns all Products with the given name
+
+        :param name: the name of the Product you want to match
+        :type name: str
+
+        :return: a collection of Products with that name
+        :rtype: list
+
+        """
+        logger.info("Processing product name query for %s ...", name)
         return cls.query.filter(cls.name == name)
+
+    @classmethod
+    def find_by_category(cls, category: str) -> list:
+        """Returns all of the Products in a category
+
+        :param category: the category of the Products you want to match
+        :type category: str
+
+        :return: a collection of Products in that category
+        :rtype: list
+
+        """
+        logger.info("Processing product category query for %s ...", category)
+        return cls.query.filter(cls.category == category)
+
+    @classmethod
+    def find_by_price(cls, price: float) -> list:
+        """Returns all of the Products with a given price
+
+        :param category: the price of the Products you want to match
+        :type category: float
+
+        :return: a collection of Products with that price
+        :rtype: list
+
+        """
+        logger.info("Processing product price query for %s ...", price)
+        return cls.query.filter(cls.price == price)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,37 +1,285 @@
 """
-Test cases for YourResourceModel Model
+Test cases for Product Model
+
+Test cases can be run with:
+    nosetests
+    coverage report -m
+
+While debugging just these tests it's convenient to use this:
+    nosetests --stop tests/test_products.py:TestProductModel
 
 """
 import os
 import logging
 import unittest
-from service.models import YourResourceModel, DataValidationError, db
+from datetime import date
+from werkzeug.exceptions import NotFound
+from service.models import Product, DataValidationError, db
+from service import app
+from tests.factories import ProductFactory
 
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
+)
 
 ######################################################################
-#  YourResourceModel   M O D E L   T E S T   C A S E S
+#  P R O D U C T   M O D E L   T E S T   C A S E S
 ######################################################################
-class TestYourResourceModel(unittest.TestCase):
-    """ Test Cases for YourResourceModel Model """
+
+class TestProductModel(unittest.TestCase):
+    """ Test Cases for ProductModel Model """
 
     @classmethod
     def setUpClass(cls):
         """ This runs once before the entire test suite """
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        Product.init_db(app)
 
     @classmethod
     def tearDownClass(cls):
         """ This runs once after the entire test suite """
+        db.session.close()
 
     def setUp(self):
         """ This runs before each test """
+        db.session.query(Product).delete()  # clean up the last tests
+        db.session.commit()
 
     def tearDown(self):
         """ This runs after each test """
+        db.session.remove()
 
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
 
-    def test_example_replace_this(self):
-        """ It should always be true """
-        self.assertTrue(True)
+    def test_create_a_product(self):
+        """It should Create a product and assert that it exists"""
+        product = Product(name="Cheese", price=6.5, category="dairy", inventory=5, created_date=date.today())
+        self.assertEqual(str(product), "<Product Cheese id=[None]>")
+        self.assertTrue(product is not None)
+        self.assertEqual(product.id, None)
+        self.assertEqual(product.name, "Cheese")
+        self.assertEqual(product.price, 6.5)
+        self.assertEqual(product.category, "dairy")
+        self.assertEqual(product.inventory, 5)
+        self.assertEqual(product.created_date, date.today())
+        product = Product(name="Cheese", desc="This is more popular", price=7.5, category="dairy", inventory=10, created_date=date.today())
+        self.assertEqual(product.desc, "This is more popular")
+        self.assertEqual(product.price, 7.5)
+        self.assertEqual(product.inventory, 10)
+
+    def test_add_a_product(self):
+        """It should Create a product and add it to the database"""
+        products = Product.all()
+        self.assertEqual(products, [])
+        product = Product(name="Cheese", price=6.5, category="dairy", inventory=5, created_date=date.today())
+        self.assertTrue(product is not None)
+        self.assertEqual(product.id, None)
+        product.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(product.id)
+        products = Product.all()
+        self.assertEqual(len(products), 1)
+
+    def test_read_a_product(self):
+        """It should Read a Product"""
+        product = ProductFactory()
+        logging.debug(product)
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        # Fetch it back
+        found_product = Product.find(product.id)
+        self.assertEqual(found_product.id, product.id)
+        self.assertEqual(found_product.name, product.name)
+        self.assertEqual(found_product.desc, product.desc)
+        self.assertEqual(found_product.price, product.price)
+        self.assertEqual(found_product.category, product.category)
+        self.assertEqual(found_product.inventory, product.inventory)
+        self.assertEqual(found_product.discount, product.discount)
+        self.assertEqual(found_product.created_date, product.created_date)
+
+    def test_update_a_product(self):
+        """It should Update a Product"""
+        product = ProductFactory()
+        logging.debug(product)
+        product.id = None
+        product.create()
+        logging.debug(product)
+        self.assertIsNotNone(product.id)
+        # Change it an save it
+        product.price = 8
+        original_id = product.id
+        product.update()
+        self.assertEqual(product.id, original_id)
+        self.assertEqual(product.price, 8)
+        # Fetch it back and make sure the id hasn't changed
+        # but the data did change
+        products = Product.all()
+        self.assertEqual(len(products), 1)
+        self.assertEqual(products[0].id, original_id)
+        self.assertEqual(products[0].price, 8)
+
+    def test_update_no_id(self):
+        """It should not Update a Product with no id"""
+        product = ProductFactory()
+        logging.debug(product)
+        product.id = None
+        self.assertRaises(DataValidationError, product.update)
+
+    def test_delete_a_product(self):
+        """It should Delete a Product"""
+        product = ProductFactory()
+        product.create()
+        self.assertEqual(len(Product.all()), 1)
+        # delete the product and make sure it isn't in the database
+        product.delete()
+        self.assertEqual(len(Product.all()), 0)
+
+    def test_list_all_products(self):
+        """It should List all Products in the database"""
+        products = Product.all()
+        self.assertEqual(products, [])
+        # Create 5 Products
+        for _ in range(5):
+            product = ProductFactory()
+            product.create()
+        # See if we get back 5 products
+        products = Product.all()
+        self.assertEqual(len(products), 5)
+
+    def test_serialize_a_product(self):
+        """It should serialize a Product"""
+        product = ProductFactory()
+        data = product.serialize()
+        self.assertNotEqual(data, None)
+        self.assertIn("id", data)
+        self.assertEqual(data["id"], product.id)
+        self.assertIn("name", data)
+        self.assertEqual(data["name"], product.name)
+        self.assertIn("desc", data)
+        self.assertEqual(data["desc"], product.desc)
+        self.assertIn("price", data)
+        self.assertEqual(data["price"], product.price)
+        self.assertIn("category", data)
+        self.assertEqual(data["category"], product.category)
+        self.assertIn("inventory", data)
+        self.assertEqual(data["inventory"], product.inventory)
+        self.assertIn("discount", data)
+        self.assertEqual(data["discount"], product.discount)
+        self.assertIn("created_date", data)
+        self.assertEqual(date.fromisoformat(data["created_date"]), product.created_date)
+        self.assertIn("modified_date", data)
+        self.assertEqual(date.fromisoformat(data["modified_date"]), product.modified_date)
+        self.assertIn("deleted_date", data)
+        self.assertEqual(date.fromisoformat(data["deleted_date"]), product.deleted_date)
+
+    def test_deserialize_a_product(self):
+        """It should de-serialize a Product"""
+        data = ProductFactory().serialize()
+        product = Product()
+        product.deserialize(data)
+        self.assertNotEqual(product, None)
+        self.assertEqual(product.id, None)
+        self.assertEqual(product.name, data["name"])
+        self.assertEqual(product.desc, data["desc"])
+        self.assertEqual(product.price, data["price"])
+        self.assertEqual(product.category, data["category"])
+        self.assertEqual(product.inventory, data["inventory"])
+        self.assertEqual(product.discount, data["discount"])
+        self.assertEqual(product.created_date, date.fromisoformat(data["created_date"]))
+        self.assertEqual(product.modified_date, date.fromisoformat(data["modified_date"]))
+        self.assertEqual(product.deleted_date, date.fromisoformat(data["deleted_date"]))
+
+    def test_deserialize_missing_data(self):
+        """It should not deserialize a Product with missing data"""
+        data = {"id": 1, "name": "Tea", "category": "cat"}
+        product = Product()
+        self.assertRaises(DataValidationError, product.deserialize, data)
+
+    def test_deserialize_bad_data(self):
+        """It should not deserialize bad data"""
+        data = "this is not a dictionary"
+        product = Product()
+        self.assertRaises(DataValidationError, product.deserialize, data)
+
+    def test_find_product(self):
+        """It should Find a Product by ID"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        logging.debug(products)
+        # make sure they got saved
+        self.assertEqual(len(Product.all()), 5)
+        # find the 2nd product in the list
+        product = Product.find(products[1].id)
+        self.assertIsNot(product, None)
+        self.assertEqual(product.id, products[1].id)
+        self.assertEqual(product.name, products[1].name)
+        self.assertEqual(product.desc, products[1].desc)
+        self.assertEqual(product.price, products[1].price)
+        self.assertEqual(product.category, products[1].category)
+        self.assertEqual(product.inventory, products[1].inventory)
+        self.assertEqual(product.discount, products[1].discount)
+        self.assertEqual(product.created_date, products[1].created_date)
+
+    def test_find_by_category(self):
+        """It should Find Products by Category"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        category = products[0].category
+        count = len([product for product in products if product.category == category])
+        found = Product.find_by_category(category)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.category, category)
+
+    def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
+
+    def test_find_by_price(self):
+        """It should Find Products by Price"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        price = products[0].price
+        count = len([product for product in products if product.price == price])
+        found = Product.find_by_price(price)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.price, price)
+
+    def test_find_or_404_found(self):
+        """It should Find or return 404 not found"""
+        products = ProductFactory.create_batch(3)
+        for product in products:
+            product.create()
+
+        product = Product.find_or_404(products[1].id)
+        self.assertIsNot(product, None)
+        self.assertEqual(product.id, products[1].id)
+        self.assertEqual(product.name, products[1].name)
+        self.assertEqual(product.desc, products[1].desc)
+        self.assertEqual(product.price, products[1].price)
+        self.assertEqual(product.category, products[1].category)
+        self.assertEqual(product.inventory, products[1].inventory)
+        self.assertEqual(product.discount, products[1].discount)
+        self.assertEqual(product.created_date, products[1].created_date)
+
+    def test_find_or_404_not_found(self):
+        """It should return 404 not found"""
+        self.assertRaises(NotFound, Product.find_or_404, 0)
