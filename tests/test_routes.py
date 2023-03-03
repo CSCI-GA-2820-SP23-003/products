8fc9d2a49cfc0efd69726f4201a0681ed83a4cc0
Ruoyu Gao <ruoyu.gao@nyu.edu>
2023-03-03 15:59:32 -0500

    provide read API and test(provide create API temp inorder to read)

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,10 +10,14 @@ import logging
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 from service import app
-from service.models import db
+from service.models import db, init_db, Product
 from service.common import status  # HTTP Status Codes
+from tests.factories import ProductFactory
 
-
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
+)
+BASE_URL = "/products"
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
@@ -23,17 +27,41 @@ class TestYourResourceServer(TestCase):
     @classmethod
     def setUpClass(cls):
         """ This runs once before the entire test suite """
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        # Set up the test database
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        init_db(app)
 
     @classmethod
     def tearDownClass(cls):
         """ This runs once after the entire test suite """
+        db.session.close()
 
     def setUp(self):
         """ This runs before each test """
-        self.app = app.test_client()
+        self.client = app.test_client()
+        db.session.query(Product).delete()  # clean up the last tests
+        db.session.commit()
 
     def tearDown(self):
         """ This runs after each test """
+        db.session.remove()
+
+    def _create_products(self, count):
+        """Factory method to create pets in bulk"""
+        products = []
+        for _ in range(count):
+            test_product = ProductFactory()
+            response = self.client.post(BASE_URL, json=test_product.serialize())
+            self.assertEqual(
+                response.status_code, status.HTTP_201_CREATED, "Could not create test product"
+            )
+            new_product = response.get_json()
+            test_product.id = new_product["id"]
+            products.append(test_product)
+        return products
 
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
@@ -41,5 +69,23 @@ class TestYourResourceServer(TestCase):
 
     def test_index(self):
         """ It should call the home page """
-        resp = self.app.get("/")
+        resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+
+    def test_get_product(self):
+        """It should Get a single Product"""
+        # get the id of a pet
+        test_product = self._create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
+
+    def test_get_product_not_found(self):
+        """It should not Get a Product thats not found"""
+        response = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        data = response.get_json()
+        logging.debug("Response data = %s", data)
+        self.assertIn("was not found", data["message"])
