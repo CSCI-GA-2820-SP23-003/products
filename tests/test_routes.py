9e4b3caefc29fd8454a75e0f4c36e2f9f0202a8d
Hanhlin <hanhsuan.lin0615@gmail.com>
2023-03-07 20:00:56 +0000

    remove unused import

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,9 +7,9 @@ Test cases can be run with the following:
 """
 import os
 import logging
-from datetime import date, datetime, timedelta
+from datetime import timedelta
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
+# from unittest.mock import MagicMock, patch
 from service import app
 from service.models import db, init_db, Product
 from service.common import status  # HTTP Status Codes
