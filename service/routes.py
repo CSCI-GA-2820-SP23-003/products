80bd4d61c48d3b9805252384f2cfc9a57786a1a8
Kranthi Kiran GV <kranthi.gv@gmail.com>
2023-03-07 17:57:08 -0500

    Remove unused imports in service

--- service/routes.py
+++ service/routes.py
@@ -7,7 +7,7 @@ GET /products/{id} - Returns the Product with a given id number
 POST /products - creates a new Product record in the database
 """
 
-from flask import Flask, jsonify, request, url_for, make_response, abort
+from flask import jsonify, request, url_for, abort
 from service.common import status  # HTTP Status Codes
 from service.models import Product
 
